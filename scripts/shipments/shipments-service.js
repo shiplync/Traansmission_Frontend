a8f3392c80f19e1826e4c5f09649ae3164f9dfa0
shiplync <webmaster@ieiplastics.com>
2017-10-03 17:55:44 +0530

    Update shipments-service.js

--- scripts/shipments/shipments-service.js
+++ scripts/shipments/shipments-service.js
@@ -22,8 +22,8 @@ angular.module('shippersPortalApp.shipments')
 
   apis.getShipment = function(shipment_id) {return baseService.getObject(shipment_conf, shipment_id)};
   apis.getPaginatedShipments = function(params){return baseService.getPaginatedObjects(shipment_conf, params)};
- // apis.postShipment = function(data){return baseService.postObject(shipment_conf, data)};
-  apis.postShipment = function(data){return baseService.postObject(msg_conf, data)};
+  apis.postShipment = function(data){return baseService.postObject(shipment_conf, data)};
+  apis.getMessage = function(data){return baseService.postObject(msg_conf, data)};
   apis.patchShipment = function(data, shipment_id){return baseService.patchObject(shipment_conf, data, shipment_id)};
   apis.deleteShipment = function(shipment_id){return baseService.deleteObject(shipment_conf, shipment_id)};
 
